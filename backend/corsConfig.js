20140dbb6060e83a1ddbc8f5370aab30cf3569b0
Priyanshu Kumar <priyanshukr19@gmail.com>
2024-07-22 13:18:14 +0530

    Separeted backend directory

--- backend/corsConfig.js
+++
@@ -1,6 +0,0 @@
-const allowedOrigins = [
-    'https://localhost:3000',
-    'https://192.168.1.8:3000'
-  ];
-  
-export default allowedOrigins;
--- backend/models/models.js
+++
@@ -1,5 +0,0 @@
-// models/index.js
-export { default as OpenOrder } from './OpenOrders.js';
-export { default as ClosedOrder } from './ClosedOrders.js';
-export { default as Stock } from './Stocks.js';
-export { default as User } from './Users.js';
